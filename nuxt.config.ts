4028d8ed8dc84481ea629ba6221d0973193dbbb9
takashirakki <stgtbike4649@gmail.com>
2024-08-17 10:18:05 +0000

    add: tailwind css.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   routeRules: {
     "/account/**": { ssr: false },
     "/auth/**": { ssr: false },
