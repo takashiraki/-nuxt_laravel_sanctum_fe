83d17b5b67d9ae01f6b5fc9fd50c55183bfa5d6e
takashirakki <stgtbike4649@gmail.com>
2024-08-17 10:47:15 +0000

    add: basic login view.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+  components: [
+    {
+      path: "./components",
+      pathPrefix: false,
+    },
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},
