74428f5371f80fe6d7b4034753c0a2f8879cfe71
takashirakki <stgtbike4649@gmail.com>
2024-08-18 05:50:14 +0000

    add: signout and user info.

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,3 +1,49 @@
+<script setup lang="ts">
+import { data } from "autoprefixer";
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+} from "~/components/ui/card";
+
+definePageMeta({
+  middleware: "auth",
+});
+
+const { user } = useAuth();
+
+const { $sanctumAuth } = useNuxtApp();
+
+const logout = async () => {
+  await $sanctumAuth.logout((data) => {
+    navigateTo({
+      path: "/auth/signin",
+    });
+  });
+};
+</script>
 <template>
-  <div>home</div>
+  <div class="h-screen flex justify-center items-center">
+    <Card class="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle class="text-2xl"> home </CardTitle>
+        <CardDescription>
+          This is the home screen. Here it is possible to check user information
+          and log out.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Id : {{ user.id }}</p>
+        <p>User name : {{ user.name }}</p>
+        <p>email : {{ user.email }}</p>
+      </CardContent>
+      <CardFooter>
+        <nuxt-link to="/auth/signin">
+          <Button v-on:click="logout"> Sign out </Button>
+        </nuxt-link>
+      </CardFooter>
+    </Card>
+  </div>
 </template>
