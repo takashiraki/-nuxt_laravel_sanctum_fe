8f644b8ee4c91790852b94edbf994c8681dd9783
takashirakki <stgtbike4649@gmail.com>
2024-09-09 00:24:02 +0000

    delete: useless code.

--- pages/auth/signin/index.vue
+++ pages/auth/signin/index.vue
@@ -1,60 +1,14 @@
 <script setup lang="ts">
-import { toTypedSchema } from "@vee-validate/zod";
-import { configure, useForm } from "vee-validate";
-import { z } from "zod";
 import SignInTemplate from "~/components/template/auth/signin/SignInTemplate.vue";
 import type { CardItemInterface } from "~/types/card/CardItemInterface";
 import type { InputItemInterface } from "~/types/form/InputItemInterface";
-
-configure({
-  validateOnBlur: false,
-  validateOnChange: false,
-});
 
 definePageMeta({
   middleware: "guest",
 });
 
-const { $sanctumAuth } = useNuxtApp();
-
-const loadingStatus = ref(false);
-
-const credentialError = ref("");
-
-const formSchema = toTypedSchema(
-  z.object({
-    email: z.string().email().min(1).max(256),
-    password: z.string().min(3).max(256),
-  })
-);
-
-const { handleSubmit } = useForm({
-  validationSchema: formSchema,
-  initialValues: {
-    email: "",
-    password: "",
-  },
-});
-
-const login = handleSubmit(async (values) => {
-  try {
-    loadingStatus.value = true;
-    credentialError.value = "";
-    await $sanctumAuth.login(
-      {
-        email: values.email,
-        password: values.password,
-      },
-      () => {
-        navigateTo({
-          path: "/home",
-        });
-      }
-    );
-  } catch (error: any) {
-    loadingStatus.value = false;
-    credentialError.value = "Credential error.";
-  }
+useHead({
+  title: "Sign in",
 });
 
 const cardItem: CardItemInterface = {
