ef62f26948705cb22639aada80eaed3a83122ae8
takashirakki <stgtbike4649@gmail.com>
2024-09-10 00:10:19 +0000

    update: import state

--- components/atom/button/ButtonAtom.vue
+++ components/atom/button/ButtonAtom.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { Loader2 } from "lucide-vue-next";
+import { useButtonLoadingState } from "~/composables/states/button/loadingButtonState";
+
 interface ButtonItemInterface {
   type: string;
   label: string;
@@ -6,10 +9,20 @@ interface ButtonItemInterface {
 }
 
 const propsItem = defineProps<ButtonItemInterface>();
+
+const buttonLoadingState = useButtonLoadingState();
 </script>
 <template>
   <div>
-    <Button :class="propsItem.class" :type="propsItem.type">
+    <Button
+      :class="propsItem.class"
+      :type="propsItem.type"
+      v-bind:disabled="buttonLoadingState.buttonLoadingState.value"
+    >
+      <Loader2
+        v-if="buttonLoadingState.buttonLoadingState.value"
+        class="w-4 h-4 mr-2 animate-spin"
+      />
       {{ propsItem.label }}</Button
     >
   </div>
---
+++ components/molecules/form/SignInPassword.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import type { InputItemInterface } from "~/types/form/InputItemInterface";
+interface passwordListItem {
+  title: string;
+  link: string;
+}
+
+const props = defineProps<{
+  item: InputItemInterface;
+  reset: passwordListItem;
+}>();
+</script>
+<template>
+  <div>
+    <FormField v-slot="{ componentField }" :name="props.item.name">
+      <FormItem>
+        <div class="flex items-center">
+          <Label>{{ props.item.label }}</Label>
+          <a
+            :href="props.reset.link"
+            class="ml-auto inline-block text-sm underline"
+          >
+            {{ props.reset.title }}
+          </a>
+        </div>
+        <FormControl>
+          <Input
+            :type="props.item.type"
+            :placeholder="props.item.placeholder"
+            v-bind="componentField"
+            required
+          />
+        </FormControl>
+        <FormMessage />
+      </FormItem>
+    </FormField>
+  </div>
+</template>
